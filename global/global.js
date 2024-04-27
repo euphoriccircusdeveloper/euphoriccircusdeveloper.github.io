3c09f2ffb8b84c8bbbca83a0ceb77e384f86777d
L-Holmes <lah21@outlook.com>
2024-04-27 11:55:26 +0100

    testing new 'server needed' error

---
+++ global/global.js
@@ -0,0 +1,61 @@
+
+export async function setDynamicElementsContent(){
+	var prefix = "D-"
+	var corrContentFuncNameStart = "contentGetter_"
+
+	console.log("Loading the dynamic elements...")
+
+	//get all elements that have an id that starts with the prefix that identifies that element as an element with dymaic content
+	const dynamicElements = document.querySelectorAll('[id^="' + prefix + '"]');
+
+	for (let i = 0; i < dynamicElements.length; i++) {
+		//--GET THE ELEMENT--
+		var element = dynamicElements[i];
+
+		//--GET ITS ID--
+        const id = element.id;
+
+		console.log("Setting the inner html to loading...")
+		//--SET THAT ELEMENT AS LOADING--
+		element.innerHTML = "Loading...";
+
+		//--LOAD EXPECTED FUNCTION NAME--
+
+		//the end of the corresponding content function name 
+        const corrContentFuncNameEnd = id.replace(new RegExp('^' + prefix), '').replace(/-/g, '_'); // Remove the prefix from the start and replace dashes with underscores
+		//corresponding content function name
+		const corrContentFuncName = corrContentFuncNameStart + corrContentFuncNameEnd
+
+		//--ENSURE FUNCTION EXISTS--
+		if (!window[corrContentFuncName] || typeof window[corrContentFuncName] !== 'function') {
+			console.error(`There is no function with the name ${corrContentFuncName}. It was expected that this function exists, since you have an element with the id: ${id}, which is seen to be representing a dynamic element due to starting with the prefix ${prefix}`);
+			element.innerHTML = "No content available";
+			continue;
+		}
+
+		//--GET THE NEW HTML CONTENT USING THAT FUNCTION--
+		var elementContent = "No content available."
+		try{
+			elementContent = await window[corrContentFuncName]();
+		} catch (error) {
+			console.error(`There was an error when attempting to fetch the content of element with the id: ${id}, using its corresponding function: ${corrContentFuncName}. The error is as follows: ${error}`);
+			element.innerHTML = "No content available";
+			continue;
+		}
+
+		if (elementContent instanceof Promise || String(elementContent) === '[object Promise]') {
+			console.error(`There was an error (received a Promise, not content) when attempting to fetch the content of element with the id: ${id}, using its corresponding function: ${corrContentFuncName}.`);
+			// Output 'error' or handle the error in another way if needed
+			element.innerHTML = "No content available";
+			continue;
+		}
+		console.log(`Got the new html content by calling the function name: ${corrContentFuncName}, before receiving the response: ${elementContent}`)
+
+		//--SET THE ELEMENTS NEW CONTENT--
+		element.innerHTML = elementContent;
+    }
+}
+
+
+//export all functions
+//window.setDynamicElementsContent = setDynamicElementsContent;
--- newsletters.js
+++ newsletters.js
@@ -1,5 +1,31 @@
+import { setDynamicElementsContent } from 'global/global.js';
+
+document.addEventListener("DOMContentLoaded", setDynamicElementsContent);
+
+//--content getters--
+
+async function contentGetter_text_lines() {
+	return "<h1> This surrrreeellllyyyy is the dynamic content </h1>"
+}
 
 
+async function contentGetter_loaded_txt() {
+	try{
+		fileContents = loadFileContents()
+	} catch (error) {
+		throw error;
+	}
+	return `${fileContents}`
+}
+
+//////////////////////////
+//GROTE FUNCTIONS
+//////////////////////////
+
+
+//////////////////////////
+//KLEINE FUNCTIONS
+//////////////////////////
 
 /**
  * Load the contents of a text file asynchronously.
@@ -11,6 +37,7 @@ async function loadFileContents(filePath) {
 		const response = await fetch(filePath);
 		if (!response.ok) {
 			throw new Error(`Error fetching file: ${response.status} ${response.statusText}`);
+			return null
 		}
 		const fileContents = await response.text();
 		return fileContents;
@@ -28,58 +55,3 @@ async function loadFileContents(filePath) {
 function splitIntoLines(fileContents) {
 	return fileContents.split('\n');
 }
-
-///////////////////////
-// SPECIAL FUNCTIONS 
-//////////////////////
-
-/**
- * This is basically returning alpine.js code
- * When placed inside of an alpine.js x-data, this function:
- * 	--> allows for the variables defined in this function to be accessible to the alpine-js functions.
- * 	--> allows for the function init(), defined in this function, to be accessible to the alpine-js functions.
-**/
-function fetchVars(){
-	console.log("FETCHING VARIABLES AND FETCHING INIT FUNCTION")
-	return {
-		loading: true, // Initially set to true
-		lines: ["hi"],
-
-		async initing() {
-			console.log("INITING!")
-			try {
-				const filePath = 'text.txt'; // Adjust the file path
-				console.log("waiting for load file contents...")
-				const fileContents = await loadFileContents(filePath);
-				console.log("got file contents!")
-				this.lines = splitIntoLines(fileContents);
-				this.loading = false; // Set loading to false once data is loaded
-			} catch (error) {
-				console.error('Error loading file:', error);
-				// Handle any errors
-			}
-		},
-	};
-}
-
-document.addEventListener('alpine:init', () => {
-	Alpine.data('fetchVariablesAndFetchInitFunction', () => ({
-		loading: true,
-		lines: ["bye"],
-
-		async init() {
-			console.log("INITING!")
-			try {
-				const filePath = 'text.txt'; // Adjust the file path
-				console.log("waiting for load file contents...")
-				const fileContents = await loadFileContents(filePath);
-				console.log("got file contents!")
-				this.lines = splitIntoLines(fileContents);
-				this.loading = false; // Set loading to false once data is loaded
-			} catch (error) {
-				console.error('Error loading file:', error);
-				// Handle any errors
-			}
-		},
-	}))
-})
