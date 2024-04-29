3c9aa304b660f00757af00ee2826dcdf8739c82d
L-Holmes <lah21@outlook.com>
2024-04-29 19:50:47 +0100

    testing gallery images

--- gallery.js
+++ gallery.js
@@ -1,12 +1,18 @@
-window.onload = function() {
-    // Fetch images from /static/images/gallery directory
-    fetch('/static/images/gallery')
-      .then(response => response.json())
-      .then(data => {
-        // Assuming the response is an array of image URLs
-        if (Array.isArray(data)) {
-          // Assign images to the Alpine.js data property
-          Alpine.store('images', data);
-        }
-      });
-  }
+document.addEventListener('alpine:init', () => {
+    Alpine.data('gallery', () => ({
+      images: [],
+
+      init() {
+        // Fetch images from /static/images/gallery directory
+        fetch('global/assets/images/page-images/gallery-images')
+          .then(response => response.json())
+          .then(data => {
+            // Assuming the response is an array of image URLs
+            if (Array.isArray(data)) {
+              // Assign images to the Alpine.js data property
+              this.images = data;
+            }
+          });
+      }
+    }));
+  });
