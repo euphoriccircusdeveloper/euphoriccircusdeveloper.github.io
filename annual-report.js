d46af5506608b90ab597d3b1f0d9745c97858642
L-Holmes <lah21@outlook.com>
2024-05-21 22:41:25 +0100

    testing the pdfs for mobile@

--- annual-report.js
+++ annual-report.js
@@ -26,3 +26,33 @@ setTimeout(function() {
 	}
 }, 1000);
 */
+
+//////////////////////
+
+var url = 'https://l-holmes.github.io/shared/images/reports/report.pdf';
+
+// Load PDF
+var loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(function(pdf) {
+// Fetch the first page
+pdf.getPage(1).then(function(page) {
+  var scale = 1.5;
+  var viewport = page.getViewport({ scale: scale });
+
+  // Prepare canvas using PDF page dimensions
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  document.getElementById('pdf-container').appendChild(canvas);
+
+  // Render PDF page into canvas context
+  var renderContext = {
+	canvasContext: context,
+	viewport: viewport
+  };
+  page.render(renderContext);
+});
+}, function (reason) {
+console.error(reason);
+});
