6aa262f3cf76099e4d4a4c4cb9b9cd9f5233b309
L-Holmes <lah21@outlook.com>
2024-08-04 18:34:45 +0100

    added dynamic getting of the contact us details

--- gallery.js
+++ gallery.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", setDynamicElementsContent);
-
-
 async function setDynamicElementsContent(){
 	var prefix = "D-"
 	var corrContentFuncNameStart = "contentGetter_"
